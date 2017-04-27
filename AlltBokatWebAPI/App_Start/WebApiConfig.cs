506ca34f3317ba09b17e295c308cb337cda6cbc1
David Nyqvist <david.nyqvist@hotmail.com>
2017-04-28 00:16:26 +0200

    grejar med log in

--- AlltBokatWebAPI/App_Start/WebApiConfig.cs
+++ AlltBokatWebAPI/App_Start/WebApiConfig.cs
@@ -13,15 +13,20 @@ namespace AlltBokatWebAPI
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-            // Configure Web API to use only bearer token authentication.
-            config.SuppressDefaultHostAuthentication();
-            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
             var cors = new EnableCorsAttribute("*", "*", "*");
             config.EnableCors(cors);
+
+
+
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
